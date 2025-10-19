cf853f6f857ce66813fba4ea45421dc9e1bfd9b2
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-10-19 20:06:44 -0300

    Added reference to Redis cache in api via AppHost

--- 05-activity/AppHost/Program.cs
+++ 05-activity/AppHost/Program.cs
@@ -2,7 +2,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var myCache = builder.AddRedis("cache");
 
-var myApi = builder.AddProject<Projects.Api>("api");
+var myApi = builder.AddProject<Projects.Api>("api")
+    .WithReference(myCache);
 
 var web = builder.AddProject<Projects.MyWeatherHub>("myweatherhub")
               .WithReference(myApi)
